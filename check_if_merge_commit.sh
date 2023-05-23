7aef515adc855e45f468361b8b7d475e4def08f6
mmatotan <marko.matotan@kodius.com>
2023-05-23 10:28:18 +0200

    Add the action

--- check_if_merge_commit.sh
+++
@@ -1,5 +0,0 @@
-if [[ $(git show --no-patch --format="%P" $GITHUB_SHA | wc -w) == "2" ]]; then
-  echo "is_merge_commit=true" >> $GITHUB_OUTPUT
-else
-  echo "is_merge_commit=false" >> $GITHUB_OUTPUT
-fi
